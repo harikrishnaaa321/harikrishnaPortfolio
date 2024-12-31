a7af921e7ed2fa8d1c29568ee8a0a390dec8f5b2
Hari Krishna Battula <142043674+harikrishnaaa321@users.noreply.github.com>
2024-12-31 19:05:24 +0530

    Update Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,46 +1,46 @@
-const ComputersCanvas = () => {
-  const [isMobile, setIsMobile] = useState(false);
+// const ComputersCanvas = () => {
+//   const [isMobile, setIsMobile] = useState(false);
 
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 768px)");
-    setIsMobile(mediaQuery.matches);
+//   useEffect(() => {
+//     const mediaQuery = window.matchMedia("(max-width: 768px)");
+//     setIsMobile(mediaQuery.matches);
 
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
-    };
+//     const handleMediaQueryChange = (event) => {
+//       setIsMobile(event.matches);
+//     };
 
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
+//     mediaQuery.addEventListener("change", handleMediaQueryChange);
 
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
-  }, []);
+//     return () => {
+//       mediaQuery.removeEventListener("change", handleMediaQueryChange);
+//     };
+//   }, []);
 
-  if (isMobile) {
-    // Render a simpler placeholder for mobile devices
-    return (
-      <div className="w-full h-[300px] flex items-center justify-center text-white">
-        <p>3D View Unavailable on Mobile</p>
-      </div>
-    );
-  }
-  return (
-    <Canvas
-      frameloop="demand"
-      shadows
-      dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Computers isMobile={isMobile} />
-      </Suspense>
-      <Preload all />
-    </Canvas>
-  );
-};
+//   if (isMobile) {
+//     // Render a simpler placeholder for mobile devices
+//     return (
+//       <div className="w-full h-[300px] flex items-center justify-center text-white">
+//         <p>3D View Unavailable on Mobile</p>
+//       </div>
+//     );
+//   }
+//   return (
+//     <Canvas
+//       frameloop="demand"
+//       shadows
+//       dpr={[1, 2]}
+//       camera={{ position: [20, 3, 5], fov: 25 }}
+//       gl={{ preserveDrawingBuffer: true }}
+//     >
+//       <Suspense fallback={<CanvasLoader />}>
+//         <OrbitControls
+//           enableZoom={false}
+//           maxPolarAngle={Math.PI / 2}
+//           minPolarAngle={Math.PI / 2}
+//         />
+//         <Computers isMobile={isMobile} />
+//       </Suspense>
+//       <Preload all />
+//     </Canvas>
+//   );
+// };
