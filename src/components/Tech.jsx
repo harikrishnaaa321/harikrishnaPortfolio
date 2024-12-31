4938dc33a252e189bb96d623750956d00d2e4e07
Hari Krishna Battula <142043674+harikrishnaaa321@users.noreply.github.com>
2024-12-31 19:18:00 +0530

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -10,9 +10,9 @@ const Tech = () => {
     <div className="flex flex-col items-center justify-center mt-16">
       <h2 className={`${styles.sectionHeadText} text-center mb-8`}>Skills</h2>
 
-      <div className="flex flex-wrap justify-center gap-10">
+      <div className="overflow-x-auto flex flex-wrap justify-center gap-10 px-4">
         {technologies.map((technology) => (
-          <div className="w-28 h-28 flex justify-center items-center" key={technology.name}>
+          <div className="w-24 h-24 sm:w-28 sm:h-28 flex justify-center items-center" key={technology.name}>
             <BallCanvas icon={technology.icon} />
           </div>
         ))}
